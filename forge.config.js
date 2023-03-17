8fba5c1d07a20883a1ca2b860ddfad76e4052f61
kevin <kevinbogodo@mail.com>
2023-03-17 12:21:28 +0100

    save run build

--- forge.config.js
+++ forge.config.js
@@ -19,4 +19,19 @@ module.exports = {
       config: {},
     },
   ],
+
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+  
 };
